adca57e5977dec6ceb13164a2adbc86b9fdb727f
rikuo00o <kato-riku-pv@ynu.jp>
2025-09-08 11:52:23 +0900

    20250908

--- app.py
+++ app.py
@@ -6,4 +6,7 @@ st.write("This is a simple web application using Streamlit.")
 input_num_1 = st.number_input("Enter a number_1", value=0)
 input_num_2 = st.number_input("Enter a number_2", value=0)
 
+st.write(f"{input_num_1} + {input_num_2} = {input_num_1 + input_num_2}")
+st.write(f"{input_num_1} - {input_num_2} = {input_num_1 - input_num_2}")
 st.write(f"{input_num_1} × {input_num_2} = {input_num_1 * input_num_2}")
+st.write(f"{input_num_1} ÷ {input_num_2} = {input_num_1 / input_num_2 if input_num_2 != 0 else 'undefined (division by zero)'}")
